1c12a683ec76b2b0f3ce573e015a68d947c45d9c
Kabilan P <104247633+Kabilanp4@users.noreply.github.com>
2024-06-17 22:37:55 +0530

    Update Sum of Square Numbers.js

--- Sum of Square Numbers.js
+++ Sum of Square Numbers.js
@@ -1,10 +1,4 @@
 var judgeSquareSum = function(c) {
-    let arr = []; 
-for (let i = 0; i < c; i++) {
-  arr.push(i);
-}
-    let k=0;
-    let s=1;
  for (let i=0;i<arr.length;i++){
      for(let j=1;j<arr.length;j++){
          if(arr[i]*arr[i]+arr[j]*arr[j]===c){
